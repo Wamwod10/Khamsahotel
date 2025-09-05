d805946dc6f17203e5ac8e5c3b86e2ce2b3af0b0
Wamwod10 <shamshodochilov160@gmail.com>
2025-09-05 20:43:39 +0500

    asa

--- src/pages/mybooking/components/Succes/PaymentSuccess.jsx
+++ src/pages/mybooking/components/Succes/PaymentSuccess.jsx
@@ -42,8 +42,11 @@ const PaymentSuccess = () => {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           to: latest.email,
-          subject: "To'lov tasdiqlandi - Khamsa Hotel",
-          text: `Hurmatli ${latest.firstName}, siz ${latest.price} EUR miqdorida to'lov amalga oshirdingiz.\n\nXona: ${latest.rooms}\nSanalar: ${latest.checkIn} - ${latest.checkOut}\n\nRahmat!`,
+          subject: "Information For Invoice",
+          text: `Thank you for choosing to stay with us via Khamsahotel.uz!  Please be informed that we are a SLEEP LOUNGE located inside the airport within the transit area. In order to stay with us you must be in possession of a valid boarding pass departing from airport Tashkent. If your flight commences from Tashkent, kindly verify with your airline first if you can check-in early for your flight as you'll need to go through passport control and security before you may access our lounge. IMPORTANT NOTE:  We will never ask you for your credit card details, or share any messages with links with you via Khamsahotel.uz for online payments or reconfirmation of your reservation with sleep ’n fly. In case of any doubt about your booking status with us please check via the Khamsahotel.uz website or app only, call Khamsahotel.uz, or contact us directly on  998 95 877 24 24 tel.whatshapp.telegram , qonoqhotel@mail.ru for Tashkent International Airport reservations.  Your Reservations Team`,
+          subjectru: "Информация Для Счета",
+          textru: `Спасибо, что решили остановиться у нас через Khamsahotel.uz! Обратите внимание, что мы являемся SLEEP LOUNGE, расположенным в транзитной зоне аэропорта. Чтобы остановиться у нас, у вас должен быть действительный посадочный талон на вылет из аэропорта Ташкента. Если ваш рейс начинается в Ташкенте, пожалуйста, сначала уточните у своей авиакомпании, можете ли вы зарегистрироваться на рейс заранее, так как вам нужно будет пройти паспортный контроль и проверку безопасности, прежде чем вы сможете попасть в наш зал ожидания.
+         ВАЖНОЕ ПРИМЕЧАНИЕ: Мы никогда не попросим вас предоставить данные вашей кредитной карты или передать вам какие-либо сообщения со ссылками через Khamsahotel.uz для онлайн-платежей или повторного подтверждения вашего бронирования с sleep ’n fly. В случае возникновения сомнений относительно статуса вашего бронирования у нас, пожалуйста, проверьте его только через веб-сайт или приложение Khamsahotel.uz, позвоните в Khamsahotel.uz или свяжитесь с нами напрямую по телефону 998 95 877 24 24 tel.whatshapp.telegram, qonoqhotel@mail.ru для бронирования в международном аэропорту Ташкента. Ваша команда по бронированию`
         }),
       })
         .then((res) => res.json())
