97e0a3d4cdf9cb313a3135f558ae0e5ea7c88caf
Wamwod10 <shamshodochilov160@gmail.com>
2025-10-05 16:29:56 +0500

    some changes in indes

--- backend/index.js
+++ backend/index.js
@@ -85,10 +85,10 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   // Agar cookie kerak bo'lsa: res.setHeader("Access-Control-Allow-Credentials", "true");
-
-  if (req.method === "OPTIONS") return res.sendStatus(204);
+  if (req.method === "OPTIONS") return res.sendStatus(204); // preflight
   next();
 });
+
 
 /* ====== Health (basic) ====== */
 app.get("/", (_req, res) =>
