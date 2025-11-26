2e2960a16588ca76769a8868cd857571c74fda9f
Wamwod10 <shamshodochilov160@gmail.com>
2025-11-27 02:35:40 +0500

    indexjs

--- backend/index.js
+++ backend/index.js
@@ -1143,7 +1143,7 @@ app.use((err, req, res, _next) => {
 
 /* ====== Start ====== */
 app.listen(PORT, () => {
-  console.log(`✅ Server yaxshi ishlayapti: ${BASE_URL} (port: ${PORT})`);
+  console.log(`✅ Server juda yaxshi ishlayapti: ${BASE_URL} (port: ${PORT})`);
   console.log(
     `[BNOVO] mode=${process.env.BNOVO_AUTH_MODE} auth_url=${
       process.env.BNOVO_AUTH_URL
