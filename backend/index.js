89b71381976b8cc9faac10f1e889c524e154eebd
Wamwod10 <shamshodochilov160@gmail.com>
2026-04-30 21:49:21 +0500

    cgha

--- backend/index.js
+++ backend/index.js
@@ -1408,7 +1408,7 @@ async function startServer() {
     await ensureRoomTypes();
 
     app.listen(PORT, () => {
-  console.log(`✅ Server juda zor ishlayapti: ${BASE_URL} (port: ${PORT})`);
+  console.log(`✅ Server juda yaxshi ishlayapti: ${BASE_URL} (port: ${PORT})`);
   console.log(
     `[BNOVO] mode=${process.env.BNOVO_AUTH_MODE} auth_url=${
       process.env.BNOVO_AUTH_URL
