3d9c4d193c887b3869084610cea7f591b0d8ad64
Wamwod10 <shamshodochilov160@gmail.com>
2026-04-30 21:54:45 +0500

    cghaxc

--- backend/index.js
+++ backend/index.js
@@ -91,8 +91,6 @@ app.use((req, res, next) => {
   next();
 });
 
-startServer();
-
 /* ====== Parsers ====== */
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true, limit: "2mb" }));
